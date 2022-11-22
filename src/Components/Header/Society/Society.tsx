55eae90fd1133a8830a957b664d7490ced61fe0d
Vladimir <vov4ik.samurai@yandex.ru>
2022-11-22 23:18:12 +0300

    mem fix

--- src/Components/Header/Society/Society.tsx
+++ src/Components/Header/Society/Society.tsx
@@ -1,12 +1,14 @@
 import {Link} from "react-router-dom";
 import "../../../index.scss"
+import vk from "../../../../Image/icons/vk.svg"
+import tel from "../../../../Image/icons/telegram.svg"
 
 export default function Society() {
     return (
         <>
             <div className="society">
-                <Link to="/"><img src="../../Image/icons/vk.svg" alt="vk" className="society__img"/></Link>
-                <Link to="/"><img src="../../Image/icons/telegram.svg" alt="telegram" className="society__img"/></Link>
+                <Link to="/"><img src={vk} alt="vk" className="society__img"/></Link>
+                <Link to="/"><img src={tel} alt="telegram" className="society__img"/></Link>
             </div>
         </>
     )
