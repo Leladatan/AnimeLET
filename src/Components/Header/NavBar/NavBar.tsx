a235766d63c8bd4d5d92cab59fc33b06ca31bfb0
Vladimir <vov4ik.samurai@yandex.ru>
2022-11-25 15:28:48 +0300

    add random + functional + adaptive ^:DDDD

--- src/Components/Header/NavBar/NavBar.tsx
+++ src/Components/Header/NavBar/NavBar.tsx
@@ -1,23 +1,39 @@
 import {NavLink} from "react-router-dom";
 import "../../../index.scss"
+import {animeData} from "../../../Store/storeAnime";
+import {IAnimeItem} from "../../../Interfaces/IAnimeItem";
 
 export default function NavBar() {
+
+    const rand = (arr: IAnimeItem[]) => {
+        return Math.floor(Math.random() * arr.length)
+    }
+
+    const randIdAnime = rand(animeData) + 1
+
     return (
         <>
             <nav className="header__navbar">
                 <ul className="header__list">
                     <li className="header__list_item">
-                        <NavLink to="/" className={({ isActive }) =>
-                        isActive ? "header__list_link_active" : "header__list_link"
+                        <NavLink to="/" className={({isActive}) =>
+                            isActive ? "header__list_link_active" : "header__list_link"
                         }>
                             HOME
                         </NavLink>
                     </li>
                     <li className="header__list_item">
-                        <NavLink to="/animelist" className={({ isActive }) =>
+                        <NavLink to="/animelist" className={({isActive}) =>
                             isActive ? "header__list_link_active" : "header__list_link"
                         }>
                             ANIME
+                        </NavLink>
+                    </li>
+                    <li className="header__list_item">
+                        <NavLink to={`/animelist/${randIdAnime}`} className={({isActive}) =>
+                            isActive ? "header__list_link_active" : "header__list_link"
+                        }>
+                            RANDOM
                         </NavLink>
                     </li>
                 </ul>
