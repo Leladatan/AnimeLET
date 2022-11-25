6abf43c8067e677a5d86965ed025bb451ccb6ecf
Vladimir <vov4ik.samurai@yandex.ru>
2022-11-25 16:03:07 +0300

    add spinner-loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,41 @@ import Home from "./Pages/Home/Home";
 import AnimeList from "./Pages/AnimeList/AnimeList";
 import NotFound from "./Pages/NotFound/NotFound";
 import AnimePage from "./Pages/AnimePage/AnimePage";
+import {useEffect, useState} from "react";
+import {RingLoader} from "react-spinners";
+import './index.scss'
 
 export default function App() {
+
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000)
+    }, [])
+
     return (
-        <Routes>
+        <>
+            {loading ?
+                <RingLoader
+                    className="loader-spinner"
+                    color="mediumpurple"
+                    loading={loading}
+                    speedMultiplier={1}
+                    size={150}
+                /> :
 
-            <Route path="/" element={<Home/>}/>
-            <Route path="/animelist" element={<AnimeList/>}/>
-            <Route path="/animelist/:id" element={<AnimePage/>}/>
-            <Route path='*' element={<NotFound/>}/>
+                <Routes>
 
-        </Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/animelist" element={<AnimeList/>}/>
+                    <Route path="/animelist/:id" element={<AnimePage/>}/>
+                    <Route path='*' element={<NotFound/>}/>
+
+                </Routes>
+            }
+        </>
     )
 }
