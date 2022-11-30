fe30a66260c76f6947d6daaad3a8415aa542793c
Vladimir <vov4ik.samurai@yandex.ru>
2022-11-30 20:40:20 +0300

    bolb

--- src/Components/Header/NavBar/NavBar.tsx
+++ src/Components/Header/NavBar/NavBar.tsx
@@ -8,16 +8,12 @@ const NavBar: FC = () => {
 
     const [randIdAnime, setRandIdAnime] = useState<number>(0);
 
-    // setTimeout(() => {
-    //     const rand = (arr: IAnimeItem[]) => {
-    //         return Math.floor(Math.random() * arr.length);
-    //     }
-    //     setRandIdAnime(rand(animeData) + 1);
-    // }, 500)
-
-    const rand = (arr: IAnimeItem[]) => {
-        return Math.floor(Math.random() * arr.length);
-    }
+    setTimeout(() => {
+        const rand = (arr: IAnimeItem[]) => {
+            return Math.floor(Math.random() * arr.length);
+        }
+        setRandIdAnime(rand(animeData) + 1);
+    }, 500)
 
     // useEffect(() => {
     //     setRandIdAnime(rand(animeData) + 1)
